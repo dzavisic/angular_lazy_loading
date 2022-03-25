0201090388b18f0b8b4ac52d1e40d3f5e4580982
zavovkci <dakizavo@gmail.com>
2022-03-25 11:36:40 +0100

    feat(client): added api service, added card, card shimmer and grid components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './components/grid/grid.component';
+import { CardComponent } from './components/card/card.component';
+import { CardShimmerComponent } from './components/card-shimmer/card-shimmer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    CardComponent,
+    CardShimmerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
